f322d168ff30b15b377d51c2ef066cf6ae6d254a
Umut Yenidil <umut.yenidil9@gmail.com>
2026-01-15 21:54:54 +0300

    chore: init project

---
+++ src/main/java/com/umutyenidil/springmcpdemo/SpringMcpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.umutyenidil.springmcpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMcpDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umutyenidil/springmcpdemo/SpringMcpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umutyenidil.springmcpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcpDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
